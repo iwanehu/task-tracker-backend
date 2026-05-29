39964a0a979dca13b7b7bc3158e99593af52d0a4
Snayder <120640856+iwanehu@users.noreply.github.com>
2026-05-29 22:56:53 +0200

    Fix: Configurar forward-headers de framework y forzar HTTPS en OpenAPI

--- src/main/java/task_tracker/demo/config/SpringDocConfig.java
+++ src/main/java/task_tracker/demo/config/SpringDocConfig.java
@@ -2,21 +2,27 @@ package task_tracker.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SpringDocConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        // Forzamos que el servidor apunte a tu URL de Render con HTTPS nativo
+        Server productionServer = new Server()
+                .url("https://task-tracker-backend-xjox.onrender.com")
+                .description("Servidor de Producción en Render");
+
         return new OpenAPI()
+                .servers(List.of(productionServer))
                 .info(new Info()
-                        .title("TaskTracker API")
-                        .version("1.0.0")
-                        .description("Documentación oficial de la API de TaskTracker para nivel empresarial")
-                        .termsOfService("http://swagger.io/terms/")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+                        .title("Task Tracker API")
+                        .version("1.0")
+                        .description("Documentación de endpoints del Task Tracker"));
     }
 }
