67e74f9fe275d60262eb984032a4d92a5639b4fd
Snayder <120640856+iwanehu@users.noreply.github.com>
2026-06-02 23:39:19 +0200

    ajustes de parametros de accesos

--- src/main/java/task_tracker/demo/config/SecurityConfig.java
+++ src/main/java/task_tracker/demo/config/SecurityConfig.java
@@ -63,8 +63,8 @@ public class SecurityConfig {
                     var config = new org.springframework.web.cors.CorsConfiguration();
                     config.setAllowedOrigins(List.of(
                             "http://localhost:5173",
-                            "https://task-tracker-font-iwanehu-gmailcoms-projects.vercel.app",
-                            "https://task-tracker-font-74j3d0irp-iwanehu-gmailcoms-projects.vercel.app"
+                            "https://task-tracker-font.vercel.app",
+                            "https://task-tracker-font-iwanehu-gmailcoms-projects.vercel.app"
                     ));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With", "Accept"));
